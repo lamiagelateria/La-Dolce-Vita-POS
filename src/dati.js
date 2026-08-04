c833ab60a27edc75a15f1cd1a5799957e2b9066f
lamiagelateria <giuliamarto23@icloud.com>
2026-08-04 18:08:30 +0200

    Update dati.js

--- src/dati.js
+++ src/dati.js
@@ -1,8 +1,15 @@
 export const creaTavoli = () => {
 
+const zone=[
+"🍽️ Sala A",
+"🍽️ Sala B",
+"🌳 Esterno"
+];
+
 return Array.from({length:120},(_,i)=>({
 
 numero:i+1,
+zona:zone[i%3],
 stato:"libero",
 ordine:[],
 totale:0
