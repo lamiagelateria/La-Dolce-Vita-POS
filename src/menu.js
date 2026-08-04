cc92ef32d99d4388053aa0eb57d2fe2ddb1da84e
lamiagelateria <giuliamarto23@icloud.com>
2026-08-04 18:03:06 +0200

    Create menu.js

---
+++ src/menu.js
@@ -0,0 +1,54 @@
+export const menu = {
+
+pizze:[
+{nome:"Margherita",prezzo:7},
+{nome:"Marinara",prezzo:6},
+{nome:"Napoli",prezzo:8},
+{nome:"Capricciosa",prezzo:10},
+{nome:"Diavola",prezzo:9},
+{nome:"Quattro Formaggi",prezzo:10},
+{nome:"Bufalina",prezzo:11},
+{nome:"Mortadella e Pistacchio",prezzo:12},
+{nome:"La Dolce Vita Special",prezzo:14},
+{nome:"Tartufata",prezzo:15}
+],
+
+extra:[
+{nome:"Funghi",prezzo:1},
+{nome:"Prosciutto",prezzo:2},
+{nome:"Olive",prezzo:1},
+{nome:"Patatine",prezzo:1.5},
+{nome:"Burrata",prezzo:3},
+{nome:"Pistacchio",prezzo:2.5}
+],
+
+bevande:[
+{nome:"Acqua",prezzo:1.5},
+{nome:"Coca Cola",prezzo:3},
+{nome:"Birra",prezzo:4}
+],
+
+caffetteria:[
+{nome:"Caffè",prezzo:1.2},
+{nome:"Cappuccino",prezzo:2},
+{nome:"Caffè corretto",prezzo:2.5}
+],
+
+dolci:[
+{nome:"Tiramisù",prezzo:5},
+{nome:"Cheesecake",prezzo:5},
+{nome:"Panna cotta",prezzo:4}
+],
+
+gelati:[
+{nome:"Coppa gelato",prezzo:5},
+{nome:"Gelato bimbo",prezzo:3}
+],
+
+bimbi:[
+{nome:"Pizza Baby",prezzo:6},
+{nome:"Pasta al pomodoro",prezzo:6},
+{nome:"Cotoletta e patatine",prezzo:7}
+]
+
+};
