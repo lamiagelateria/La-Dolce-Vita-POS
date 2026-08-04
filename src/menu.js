82fc048c2a34906cb68318e21cfb90a31da54c27
lamiagelateria <giuliamarto23@icloud.com>
2026-08-04 19:27:22 +0200

    Update menu.js

--- src/menu.js
+++ src/menu.js
@@ -1,71 +1,157 @@
 export const menu = {
 
 pizze:[
-{nome:"Margherita",prezzo:7},
-{nome:"Marinara",prezzo:6},
-{nome:"Napoli",prezzo:8},
-{nome:"Capricciosa",prezzo:10},
-{nome:"Diavola",prezzo:9},
-{nome:"Quattro Formaggi",prezzo:10},
-{nome:"Bufalina",prezzo:11},
-{nome:"Mortadella e Pistacchio",prezzo:12},
-{nome:"La Dolce Vita Special",prezzo:14},
-{nome:"Tartufata",prezzo:15}
+
+{nome:"Margherita",prezzo:7,ingredienti:["Pomodoro","Fiordilatte","Basilico"]},
+
+{nome:"Marinara",prezzo:6,ingredienti:["Pomodoro","Aglio","Origano"]},
+
+{nome:"Napoli",prezzo:8,ingredienti:["Pomodoro","Fiordilatte","Acciughe","Capperi","Origano"]},
+
+{nome:"Diavola",prezzo:9,ingredienti:["Pomodoro","Fiordilatte","Salame piccante"]},
+
+{nome:"Capricciosa",prezzo:10,ingredienti:["Pomodoro","Fiordilatte","Prosciutto","Funghi","Carciofi","Olive","Uovo"]},
+
+{nome:"Quattro Formaggi",prezzo:10,ingredienti:["Fiordilatte","Gorgonzola","Fontina","Parmigiano"]},
+
+{nome:"Bufalina",prezzo:11,ingredienti:["Pomodoro","Mozzarella di bufala","Basilico"]},
+
+{nome:"Mortadella e Pistacchio",prezzo:12,ingredienti:["Fiordilatte","Mortadella","Crema pistacchio"]},
+
+{nome:"La Dolce Vita Special",prezzo:14,ingredienti:["Pomodoro","Bufala","Prosciutto crudo","Burrata","Basilico"]},
+
+{nome:"Tartufata",prezzo:15,ingredienti:["Fiordilatte","Crema tartufo","Funghi"]},
+
+{nome:"Prosciutto e Funghi",prezzo:9,ingredienti:["Pomodoro","Fiordilatte","Prosciutto","Funghi"]},
+
+{nome:"4 Stagioni",prezzo:10,ingredienti:["Pomodoro","Mozzarella","Prosciutto","Funghi","Carciofi","Olive"]},
+
+{nome:"Boscaiola",prezzo:11,ingredienti:["Fiordilatte","Salsiccia","Funghi"]},
+
+{nome:"Parmigiana",prezzo:10,ingredienti:["Pomodoro","Mozzarella","Melanzane","Parmigiano"]},
+
+{nome:"Ortolana",prezzo:10,ingredienti:["Mozzarella","Zucchine","Peperoni","Melanzane"]},
+
+{nome:"Tonno e Cipolla",prezzo:10,ingredienti:["Pomodoro","Mozzarella","Tonno","Cipolla"]},
+
+{nome:"Salsiccia e Friarielli",prezzo:12,ingredienti:["Mozzarella","Salsiccia","Friarielli"]},
+
+{nome:"Speck e Brie",prezzo:12,ingredienti:["Mozzarella","Speck","Brie"]},
+
+{nome:"Carbonara",prezzo:12,ingredienti:["Mozzarella","Uovo","Guanciale","Pecorino"]},
+
+{nome:"Amatriciana",prezzo:11,ingredienti:["Pomodoro","Guanciale","Pecorino"]},
+
+{nome:"Piccante",prezzo:10,ingredienti:["Pomodoro","Mozzarella","Nduja","Peperoncino"]},
+
+{nome:"Rustica",prezzo:11,ingredienti:["Mozzarella","Patate","Salsiccia"]},
+
+{nome:"Contadina",prezzo:10,ingredienti:["Pomodoro","Mozzarella","Verdure grigliate"]},
+
+{nome:"Gustosa",prezzo:12,ingredienti:["Mozzarella","Pancetta","Funghi","Scamorza"]},
+
+{nome:"Del Bosco",prezzo:13,ingredienti:["Mozzarella","Funghi porcini","Tartufo"]},
+
+{nome:"Mare",prezzo:13,ingredienti:["Pomodoro","Frutti di mare","Prezzemolo"]},
+
+{nome:"Gamberetti",prezzo:12,ingredienti:["Pomodoro","Mozzarella","Gamberetti"]},
+
+{nome:"Salame e Patatine",prezzo:10,ingredienti:["Pomodoro","Mozzarella","Salame","Patatine"]},
+
+{nome:"Americana",prezzo:10,ingredienti:["Pomodoro","Mozzarella","Wurstel","Patatine"]},
+
+{nome:"Burrata e Crudo",prezzo:13,ingredienti:["Pomodoro","Burrata","Prosciutto crudo"]}
+
 ],
 
+
 extra:[
+
 {nome:"Funghi",prezzo:1},
+
 {nome:"Prosciutto",prezzo:2},
+
 {nome:"Olive",prezzo:1},
+
 {nome:"Patatine",prezzo:1.5},
+
 {nome:"Burrata",prezzo:3},
-{nome:"Pistacchio",prezzo:2.5}
+
+{nome:"Pistacchio",prezzo:2.5},
+
+{nome:"Salame piccante",prezzo:2},
+
+{nome:"Bufala",prezzo:3},
+
+{nome:"Salsiccia",prezzo:2.5},
+
+{nome:"Tonno",prezzo:2}
+
 ],
+
 
 bevande:[
 
 {nome:"Acqua Naturale",prezzo:1.5},
+
 {nome:"Acqua Frizzante",prezzo:1.5},
 
 {nome:"Coca Cola",prezzo:3},
+
 {nome:"Coca Cola Zero",prezzo:3},
+
 {nome:"Fanta",prezzo:3},
+
 {nome:"Sprite",prezzo:3},
 
 {nome:"Birra Moretti",prezzo:4},
+
 {nome:"Heineken",prezzo:4},
-{nome:"Birra alla spina Piccola",prezzo:3.5},
-{nome:"Birra alla spina Media",prezzo:5},
 
-{nome:"Vino Rosso Calice",prezzo:4},
-{nome:"Vino Bianco Calice",prezzo:4},
-
-{nome:"Amaro",prezzo:3},
-{nome:"Grappa",prezzo:3.5}
+{nome:"Birra alla spina Media",prezzo:5}
 
 ],
+
 
 caffetteria:[
+
 {nome:"Caffè",prezzo:1.2},
+
 {nome:"Cappuccino",prezzo:2},
+
 {nome:"Caffè corretto",prezzo:2.5}
+
 ],
+
 
 dolci:[
+
 {nome:"Tiramisù",prezzo:5},
+
 {nome:"Cheesecake",prezzo:5},
+
 {nome:"Panna cotta",prezzo:4}
+
 ],
+
 
 gelati:[
+
 {nome:"Coppa gelato",prezzo:5},
+
 {nome:"Gelato bimbo",prezzo:3}
+
 ],
 
+
 bimbi:[
+
 {nome:"Pizza Baby",prezzo:6},
+
 {nome:"Pasta al pomodoro",prezzo:6},
+
 {nome:"Cotoletta e patatine",prezzo:7}
+
 ]
 
 };
