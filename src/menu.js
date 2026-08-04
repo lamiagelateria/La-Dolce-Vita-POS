a3fd11b782274e13cfa6a7a03a56ebed22f1d3d5
lamiagelateria <giuliamarto23@icloud.com>
2026-08-04 19:36:07 +0200

    Update menu.js

--- src/menu.js
+++ src/menu.js
@@ -2,65 +2,590 @@ export const menu = {
 
 pizze:[
 
-{nome:"Margherita",prezzo:7,ingredienti:["Pomodoro","Fiordilatte","Basilico"]},
+{
+nome:"Margherita",
+prezzo:7,
+ingredienti:["Pomodoro","Fiordilatte","Basilico"],
+modificabile:true
+},
 
-{nome:"Marinara",prezzo:6,ingredienti:["Pomodoro","Aglio","Origano"]},
+{
+nome:"Marinara",
+prezzo:6,
+ingredienti:["Pomodoro","Aglio","Origano"],
+modificabile:true
+},
 
-{nome:"Napoli",prezzo:8,ingredienti:["Pomodoro","Fiordilatte","Acciughe","Capperi","Origano"]},
+{
+nome:"Napoli",
+prezzo:8,
+ingredienti:["Pomodoro","Fiordilatte","Acciughe","Capperi","Origano"],
+modificabile:true
+},
 
-{nome:"Diavola",prezzo:9,ingredienti:["Pomodoro","Fiordilatte","Salame piccante"]},
+{
+nome:"Diavola",
+prezzo:9,
+ingredienti:["Pomodoro","Fiordilatte","Salame piccante"],
+modificabile:true
+},
 
-{nome:"Capricciosa",prezzo:10,ingredienti:["Pomodoro","Fiordilatte","Prosciutto","Funghi","Carciofi","Olive","Uovo"]},
+{
+nome:"Capricciosa",
+prezzo:10,
+ingredienti:["Pomodoro","Fiordilatte","Prosciutto","Funghi","Carciofi","Olive","Uovo"],
+modificabile:true
+},
 
-{nome:"Quattro Formaggi",prezzo:10,ingredienti:["Fiordilatte","Gorgonzola","Fontina","Parmigiano"]},
+{
+nome:"Quattro Formaggi",
+prezzo:10,
+ingredienti:["Fiordilatte","Gorgonzola","Fontina","Parmigiano"],
+modificabile:true
+},
 
-{nome:"Bufalina",prezzo:11,ingredienti:["Pomodoro","Mozzarella di bufala","Basilico"]},
+{
+nome:"Bufalina",
+prezzo:11,
+ingredienti:["Pomodoro","Mozzarella di bufala","Basilico"],
+modificabile:true
+},
 
-{nome:"Mortadella e Pistacchio",prezzo:12,ingredienti:["Fiordilatte","Mortadella","Crema pistacchio"]},
+{
+nome:"Prosciutto e Funghi",
+prezzo:9,
+ingredienti:["Pomodoro","Fiordilatte","Prosciutto","Funghi"],
+modificabile:true
+},
 
-{nome:"La Dolce Vita Special",prezzo:14,ingredienti:["Pomodoro","Bufala","Prosciutto crudo","Burrata","Basilico"]},
+{
+nome:"4 Stagioni",
+prezzo:10,
+ingredienti:["Pomodoro","Fiordilatte","Prosciutto","Funghi","Carciofi","Olive"],
+modificabile:true
+},
 
-{nome:"Tartufata",prezzo:15,ingredienti:["Fiordilatte","Crema tartufo","Funghi"]},
+{
+nome:"Tonno e Cipolla",
+prezzo:10,
+ingredienti:["Pomodoro","Fiordilatte","Tonno","Cipolla"],
+modificabile:true
+},
 
-{nome:"Prosciutto e Funghi",prezzo:9,ingredienti:["Pomodoro","Fiordilatte","Prosciutto","Funghi"]},
+{
+nome:"Salsiccia",
+prezzo:9,
+ingredienti:["Pomodoro","Fiordilatte","Salsiccia"],
+modificabile:true
+},
 
-{nome:"4 Stagioni",prezzo:10,ingredienti:["Pomodoro","Mozzarella","Prosciutto","Funghi","Carciofi","Olive"]},
+{
+nome:"Salsiccia e Friarielli",
+prezzo:12,
+ingredienti:["Fiordilatte","Salsiccia","Friarielli"],
+modificabile:true
+},
 
-{nome:"Boscaiola",prezzo:11,ingredienti:["Fiordilatte","Salsiccia","Funghi"]},
+{
+nome:"Ortolana",
+prezzo:10,
+ingredienti:["Fiordilatte","Zucchine","Melanzane","Peperoni"],
+modificabile:true
+},
 
-{nome:"Parmigiana",prezzo:10,ingredienti:["Pomodoro","Mozzarella","Melanzane","Parmigiano"]},
+{
+nome:"Parmigiana",
+prezzo:10,
+ingredienti:["Pomodoro","Fiordilatte","Melanzane","Parmigiano"],
+modificabile:true
+},
 
-{nome:"Ortolana",prezzo:10,ingredienti:["Mozzarella","Zucchine","Peperoni","Melanzane"]},
+{
+nome:"Americana",
+prezzo:10,
+ingredienti:["Pomodoro","Fiordilatte","Wurstel","Patatine"],
+modificabile:true
+},
 
-{nome:"Tonno e Cipolla",prezzo:10,ingredienti:["Pomodoro","Mozzarella","Tonno","Cipolla"]},
+{
+nome:"Salame e Patatine",
+prezzo:10,
+ingredienti:["Pomodoro","Fiordilatte","Salame","Patatine"],
+modificabile:true
+},
 
-{nome:"Salsiccia e Friarielli",prezzo:12,ingredienti:["Mozzarella","Salsiccia","Friarielli"]},
+{
+nome:"Boscaiola",
+prezzo:11,
+ingredienti:["Fiordilatte","Salsiccia","Funghi"],
+modificabile:true
+},
 
-{nome:"Speck e Brie",prezzo:12,ingredienti:["Mozzarella","Speck","Brie"]},
+{
+nome:"Rustica",
+prezzo:11,
+ingredienti:["Fiordilatte","Patate","Salsiccia","Rosmarino"],
+modificabile:true
+},
 
-{nome:"Carbonara",prezzo:12,ingredienti:["Mozzarella","Uovo","Guanciale","Pecorino"]},
+{
+nome:"Piccante",
+prezzo:10,
+ingredienti:["Pomodoro","Fiordilatte","Nduja","Peperoncino"],
+modificabile:true
+},
 
-{nome:"Amatriciana",prezzo:11,ingredienti:["Pomodoro","Guanciale","Pecorino"]},
+{
+nome:"Contadina",
+prezzo:10,
+ingredienti:["Pomodoro","Fiordilatte","Verdure grigliate"],
+modificabile:true
+},
 
-{nome:"Piccante",prezzo:10,ingredienti:["Pomodoro","Mozzarella","Nduja","Peperoncino"]},
+{
+nome:"Speck e Brie",
+prezzo:12,
+ingredienti:["Fiordilatte","Speck","Brie"],
+modificabile:true
+},
 
-{nome:"Rustica",prezzo:11,ingredienti:["Mozzarella","Patate","Salsiccia"]},
+{
+nome:"Carbonara",
+prezzo:12,
+ingredienti:["Fiordilatte","Uovo","Guanciale","Pecorino"],
+modificabile:true
+},
 
-{nome:"Contadina",prezzo:10,ingredienti:["Pomodoro","Mozzarella","Verdure grigliate"]},
+{
+nome:"Amatriciana",
+prezzo:11,
+ingredienti:["Pomodoro","Guanciale","Pecorino"],
+modificabile:true
+},
 
-{nome:"Gustosa",prezzo:12,ingredienti:["Mozzarella","Pancetta","Funghi","Scamorza"]},
+{
+nome:"Mortadella e Pistacchio",
+prezzo:12,
+ingredienti:["Fiordilatte","Mortadella","Crema pistacchio"],
+modificabile:true
+},
 
-{nome:"Del Bosco",prezzo:13,ingredienti:["Mozzarella","Funghi porcini","Tartufo"]},
+{
+nome:"Burrata e Crudo",
+prezzo:13,
+ingredienti:["Pomodoro","Burrata","Prosciutto crudo"],
+modificabile:true
+}
 
-{nome:"Mare",prezzo:13,ingredienti:["Pomodoro","Frutti di mare","Prezzemolo"]},
+],
+ {
+nome:"Tartufata",
+prezzo:15,
+ingredienti:["Fiordilatte","Crema tartufo","Funghi porcini"],
+modificabile:true
+},
 
-{nome:"Gamberetti",prezzo:12,ingredienti:["Pomodoro","Mozzarella","Gamberetti"]},
+{
+nome:"La Dolce Vita Special",
+prezzo:14,
+ingredienti:["Mozzarella","Prosciutto crudo","Burrata","Pistacchio"],
+modificabile:true
+},
 
-{nome:"Salame e Patatine",prezzo:10,ingredienti:["Pomodoro","Mozzarella","Salame","Patatine"]},
+{
+nome:"Gustosa",
+prezzo:12,
+ingredienti:["Fiordilatte","Pancetta","Scamorza","Funghi"],
+modificabile:true
+},
 
-{nome:"Americana",prezzo:10,ingredienti:["Pomodoro","Mozzarella","Wurstel","Patatine"]},
+{
+nome:"Del Bosco",
+prezzo:13,
+ingredienti:["Fiordilatte","Funghi porcini","Tartufo"],
+modificabile:true
+},
 
-{nome:"Burrata e Crudo",prezzo:13,ingredienti:["Pomodoro","Burrata","Prosciutto crudo"]}
+{
+nome:"Montanara",
+prezzo:12,
+ingredienti:["Pomodoro","Mozzarella","Speck","Funghi"],
+modificabile:true
+},
+
+{
+nome:"Campagnola",
+prezzo:11,
+ingredienti:["Pomodoro","Mozzarella","Salsiccia","Cipolla"],
+modificabile:true
+},
+
+{
+nome:"Calabrese",
+prezzo:11,
+ingredienti:["Pomodoro","Mozzarella","Nduja","Olive"],
+modificabile:true
+},
+
+{
+nome:"Pugliese",
+prezzo:12,
+ingredienti:["Pomodoro","Mozzarella","Burrata","Pomodorini"],
+modificabile:true
+},
+
+{
+nome:"Sorrentina",
+prezzo:12,
+ingredienti:["Pomodoro","Mozzarella","Pomodorini","Basilico"],
+modificabile:true
+},
+
+{
+nome:"Genovese",
+prezzo:11,
+ingredienti:["Mozzarella","Pesto","Pomodorini"],
+modificabile:true
+},
+
+{
+nome:"Mediterranea",
+prezzo:12,
+ingredienti:["Pomodoro","Mozzarella","Olive","Capperi","Acciughe"],
+modificabile:true
+},
+
+{
+nome:"Greca",
+prezzo:11,
+ingredienti:["Mozzarella","Feta","Olive","Pomodorini"],
+modificabile:true
+},
+
+{
+nome:"Vegetariana Premium",
+prezzo:12,
+ingredienti:["Mozzarella","Zucchine","Melanzane","Peperoni","Funghi"],
+modificabile:true
+},
+
+{
+nome:"Primavera",
+prezzo:11,
+ingredienti:["Mozzarella","Pomodorini","Rucola","Grana"],
+modificabile:true
+},
+
+{
+nome:"Rucola e Grana",
+prezzo:10,
+ingredienti:["Mozzarella","Rucola","Scaglie di Grana"],
+modificabile:true
+},
+
+{
+nome:"Fume",
+prezzo:11,
+ingredienti:["Mozzarella","Scamorza affumicata","Speck"],
+modificabile:true
+},
+
+{
+nome:"Deliziosa",
+prezzo:13,
+ingredienti:["Mozzarella","Panna","Speck","Noci"],
+modificabile:true
+},
+
+{
+nome:"Golosa",
+prezzo:13,
+ingredienti:["Mozzarella","Gorgonzola","Noci","Miele"],
+modificabile:true
+},
+
+{
+nome:"Autunno",
+prezzo:13,
+ingredienti:["Mozzarella","Zucca","Salsiccia","Rosmarino"],
+modificabile:true
+},
+
+{
+nome:"Inverno",
+prezzo:13,
+ingredienti:["Mozzarella","Speck","Patate","Scamorza"],
+modificabile:true
+},
+
+{
+nome:"Estate",
+prezzo:12,
+ingredienti:["Mozzarella","Pomodorini","Rucola","Basilico"],
+modificabile:true
+},
+
+{
+nome:"Fantasia",
+prezzo:14,
+ingredienti:["Mozzarella","Ingredienti misti dello chef"],
+modificabile:true
+},
+
+{
+nome:"Chef",
+prezzo:15,
+ingredienti:["Mozzarella","Crudo","Burrata","Tartufo"],
+modificabile:true
+},
+
+{
+nome:"Regina",
+prezzo:14,
+ingredienti:["Pomodoro","Mozzarella","Bufala","Basilico"],
+modificabile:true
+},
+
+{
+nome:"Imperiale",
+prezzo:16,
+ingredienti:["Mozzarella","Tartufo","Burrata","Prosciutto crudo"],
+modificabile:true
+}, 
+{
+nome:"Frutti di Mare",
+prezzo:14,
+ingredienti:["Pomodoro","Mozzarella","Frutti di mare","Prezzemolo"],
+modificabile:true
+},
+
+{
+nome:"Gamberetti",
+prezzo:12,
+ingredienti:["Pomodoro","Mozzarella","Gamberetti"],
+modificabile:true
+},
+
+{
+nome:"Salmone",
+prezzo:14,
+ingredienti:["Mozzarella","Salmone affumicato","Rucola"],
+modificabile:true
+},
+
+{
+nome:"Tonno Deluxe",
+prezzo:13,
+ingredienti:["Pomodoro","Mozzarella","Tonno","Cipolla rossa","Olive"],
+modificabile:true
+},
+
+{
+nome:"Mare e Monti",
+prezzo:15,
+ingredienti:["Mozzarella","Funghi","Gamberetti"],
+modificabile:true
+},
+
+{
+nome:"Norcina",
+prezzo:13,
+ingredienti:["Mozzarella","Salsiccia","Tartufo"],
+modificabile:true
+},
+
+{
+nome:"Umbra",
+prezzo:13,
+ingredienti:["Mozzarella","Tartufo","Salsiccia"],
+modificabile:true
+},
+
+{
+nome:"Tirolese",
+prezzo:12,
+ingredienti:["Mozzarella","Speck","Patate"],
+modificabile:true
+},
+
+{
+nome:"Valtellina",
+prezzo:14,
+ingredienti:["Mozzarella","Bresaola","Rucola","Grana"],
+modificabile:true
+},
+
+{
+nome:"Tricolore",
+prezzo:12,
+ingredienti:["Mozzarella","Pomodorini","Rucola","Basilico"],
+modificabile:true
+},
+
+{
+nome:"Italia",
+prezzo:13,
+ingredienti:["Pomodoro","Mozzarella di bufala","Basilico"],
+modificabile:true
+},
+
+{
+nome:"Napoli Special",
+prezzo:12,
+ingredienti:["Pomodoro","Bufala","Acciughe","Capperi"],
+modificabile:true
+},
+
+{
+nome:"Siciliana",
+prezzo:11,
+ingredienti:["Pomodoro","Olive","Capperi","Acciughe","Origano"],
+modificabile:true
+},
+
+{
+nome:"Romana",
+prezzo:10,
+ingredienti:["Pomodoro","Mozzarella","Acciughe","Origano"],
+modificabile:true
+},
+
+{
+nome:"Vesuvio",
+prezzo:12,
+ingredienti:["Pomodoro","Mozzarella","Salame piccante","Peperoncino"],
+modificabile:true
+},
+
+{
+nome:"Inferno",
+prezzo:13,
+ingredienti:["Pomodoro","Mozzarella","Nduja","Salame piccante"],
+modificabile:true
+},
+
+{
+nome:"Texas",
+prezzo:12,
+ingredienti:["Mozzarella","Wurstel","Patatine","Salsa barbecue"],
+modificabile:true
+},
+
+{
+nome:"Bavarese",
+prezzo:12,
+ingredienti:["Mozzarella","Wurstel","Cipolla"],
+modificabile:true
+},
+
+{
+nome:"Messicana",
+prezzo:13,
+ingredienti:["Pomodoro","Mozzarella","Fagioli","Peperoncino","Carne piccante"],
+modificabile:true
+},
+
+{
+nome:"Hawaiana",
+prezzo:11,
+ingredienti:["Mozzarella","Prosciutto","Ananas"],
+modificabile:true
+},
+
+{
+nome:"Bufala e Pomodorini",
+prezzo:12,
+ingredienti:["Mozzarella di bufala","Pomodorini","Basilico"],
+modificabile:true
+},
+
+{
+nome:"Burrata Gourmet",
+prezzo:15,
+ingredienti:["Burrata","Pomodorini","Rucola","Grana"],
+modificabile:true
+},
+
+{
+nome:"Pistacchio Gourmet",
+prezzo:15,
+ingredienti:["Mozzarella","Mortadella","Crema pistacchio","Granella pistacchio"],
+modificabile:true
+},
+
+{
+nome:"Oro Verde",
+prezzo:16,
+ingredienti:["Mozzarella","Burrata","Pistacchio","Prosciutto crudo"],
+modificabile:true
+},
+
+{
+nome:"Dolce Vita Suprema",
+prezzo:18,
+ingredienti:["Mozzarella","Burrata","Tartufo","Crudo","Pistacchio"],
+modificabile:true
+}
+
+],
+ingredientiDisponibili:[
+
+"Pomodoro",
+"Fiordilatte",
+"Mozzarella",
+"Mozzarella di bufala",
+"Burrata",
+"Basilico",
+"Origano",
+"Aglio",
+"Acciughe",
+"Capperi",
+"Olive",
+"Funghi",
+"Funghi porcini",
+"Prosciutto cotto",
+"Prosciutto crudo",
+"Speck",
+"Mortadella",
+"Salame",
+"Salame piccante",
+"Nduja",
+"Salsiccia",
+"Guanciale",
+"Pancetta",
+"Tonno",
+"Cipolla",
+"Cipolla rossa",
+"Gamberetti",
+"Frutti di mare",
+"Salmone affumicato",
+"Zucchine",
+"Melanzane",
+"Peperoni",
+"Friarielli",
+"Patatine",
+"Patate",
+"Carciofi",
+"Rucola",
+"Pomodorini",
+"Scamorza",
+"Scamorza affumicata",
+"Gorgonzola",
+"Fontina",
+"Parmigiano",
+"Grana",
+"Brie",
+"Feta",
+"Pesto",
+"Crema tartufo",
+"Tartufo",
+"Crema pistacchio",
+"Granella pistacchio",
+"Noci",
+"Miele",
+"Uovo",
+"Wurstel",
+"Salsa barbecue"
 
 ],
 
@@ -68,47 +593,51 @@ pizze:[
 extra:[
 
 {nome:"Funghi",prezzo:1},
-
-{nome:"Prosciutto",prezzo:2},
-
+{nome:"Prosciutto cotto",prezzo:2},
+{nome:"Prosciutto crudo",prezzo:3},
 {nome:"Olive",prezzo:1},
-
 {nome:"Patatine",prezzo:1.5},
-
 {nome:"Burrata",prezzo:3},
-
-{nome:"Pistacchio",prezzo:2.5},
-
-{nome:"Salame piccante",prezzo:2},
-
 {nome:"Bufala",prezzo:3},
+{nome:"Pistacchio",prezzo:2.5},
+{nome:"Mortadella",prezzo:2.5},
+{nome:"Speck",prezzo:2.5},
+{nome:"Salame piccante",prezzo:2},
+{nome:"Salsiccia",prezzo:2},
+{nome:"Tonno",prezzo:2},
+{nome:"Gamberetti",prezzo:3},
+{nome:"Tartufo",prezzo:4},
+{nome:"Funghi porcini",prezzo:3},
+{nome:"Rucola",prezzo:1},
+{nome:"Pomodorini",prezzo:1},
+{nome:"Grana",prezzo:1.5},
+{nome:"Gorgonzola",prezzo:1.5},
+{nome:"Uovo",prezzo:1},
+{nome:"Nduja",prezzo:2}
 
-{nome:"Salsiccia",prezzo:2.5},
-
-{nome:"Tonno",prezzo:2}
-
-],
-
-
+],  
 bevande:[
 
-{nome:"Acqua Naturale",prezzo:1.5},
-
-{nome:"Acqua Frizzante",prezzo:1.5},
+{nome:"Acqua Naturale 0,5L",prezzo:1.5},
+{nome:"Acqua Frizzante 0,5L",prezzo:1.5},
+{nome:"Acqua 1L",prezzo:2},
 
 {nome:"Coca Cola",prezzo:3},
-
 {nome:"Coca Cola Zero",prezzo:3},
-
 {nome:"Fanta",prezzo:3},
-
 {nome:"Sprite",prezzo:3},
+{nome:"The Pesca",prezzo:3},
+{nome:"The Limone",prezzo:3},
 
-{nome:"Birra Moretti",prezzo:4},
-
+{nome:"Birra Moretti Piccola",prezzo:3},
+{nome:"Birra Moretti Media",prezzo:5},
 {nome:"Heineken",prezzo:4},
+{nome:"Ichnusa",prezzo:4},
+{nome:"Birra Artigianale",prezzo:6},
 
-{nome:"Birra alla spina Media",prezzo:5}
+{nome:"Vino Rosso Calice",prezzo:4},
+{nome:"Vino Bianco Calice",prezzo:4},
+{nome:"Prosecco Calice",prezzo:5}
 
 ],
 
@@ -116,10 +645,13 @@ bevande:[
 caffetteria:[
 
 {nome:"Caffè",prezzo:1.2},
-
+{nome:"Caffè Decaffeinato",prezzo:1.5},
+{nome:"Caffè Corretto",prezzo:2.5},
 {nome:"Cappuccino",prezzo:2},
-
-{nome:"Caffè corretto",prezzo:2.5}
+{nome:"Cappuccino Deca",prezzo:2.3},
+{nome:"Orzo",prezzo:1.5},
+{nome:"Ginseng",prezzo:1.8},
+{nome:"Tè Caldo",prezzo:2}
 
 ],
 
@@ -127,31 +659,35 @@ caffetteria:[
 dolci:[
 
 {nome:"Tiramisù",prezzo:5},
-
 {nome:"Cheesecake",prezzo:5},
-
-{nome:"Panna cotta",prezzo:4}
+{nome:"Panna Cotta",prezzo:4},
+{nome:"Crema Catalana",prezzo:5},
+{nome:"Torta al Cioccolato",prezzo:5},
+{nome:"Brownie",prezzo:5},
+{nome:"Dolce della Casa",prezzo:6}
 
 ],
 
 
 gelati:[
 
-{nome:"Coppa gelato",prezzo:5},
-
-{nome:"Gelato bimbo",prezzo:3}
+{nome:"Coppa Gelato",prezzo:5},
+{nome:"Gelato Bimbo",prezzo:3},
+{nome:"Affogato al Caffè",prezzo:5},
+{nome:"Coppa Speciale",prezzo:7}
 
 ],
 
 
 bimbi:[
 
-{nome:"Pizza Baby",prezzo:6},
-
-{nome:"Pasta al pomodoro",prezzo:6},
-
-{nome:"Cotoletta e patatine",prezzo:7}
+{nome:"Pizza Baby Margherita",prezzo:6},
+{nome:"Pizza Baby Wurstel",prezzo:7},
+{nome:"Pasta al Pomodoro",prezzo:6},
+{nome:"Pasta al Ragù",prezzo:7},
+{nome:"Cotoletta e Patatine",prezzo:8},
+{nome:"Hamburger Bimbo",prezzo:8}
 
 ]
 
-};
+};  
